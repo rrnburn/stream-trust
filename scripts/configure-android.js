b031f85e433475f774d52c420f66c2298da45071
robbieb <rrnburnett@gmail.com>
2026-04-20 16:10:02 +0100

    fix fs

--- scripts/configure-android.js
+++ scripts/configure-android.js
@@ -5,8 +5,12 @@
  * Run this after: npx cap add android
  */
 
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const ANDROID_DIR = path.join(__dirname, '..', 'android');
 
